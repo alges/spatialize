703316c8a495592e6d7216b6e4757400abaaa1de
Jesu <mvalenzuela@alges.cl>
2026-07-14 12:22:40 -0400

    Bump version to 1.1.2

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ libspatialize_extensions = [
 if __name__ == '__main__':
     setup(
         name='spatialize',
-        version='1.1.1',
+        version='1.1.2',
         author='ALGES Laboratory',
         author_email='dev@alges.cl',
         description='Python Library for Generative Geostatistics and Spatial Analysis',
